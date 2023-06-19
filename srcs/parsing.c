d3ab0521dd2f8f4a1e8844993532f891768ea059
Elias MOREAU <emoreau@paul-f4Ar3s2.clusters.42paris.fr>
2023-06-19 20:12:12 +0200

    malloc

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -3,43 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elias <elias@student.42.fr>                +#+  +:+       +#+        */
+/*   By: emoreau <emoreau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 18:05:30 by emoreau           #+#    #+#             */
-/*   Updated: 2023/06/19 16:04:07 by elias            ###   ########.fr       */
+/*   Updated: 2023/06/19 20:04:56 by emoreau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../includes/push_swap.h"
-
- char    *ft_strjoin2(char const *s1, char *s2)
-{
-    char    *str;
-    size_t    i;
-    size_t    j;
-
-    if (!s1)
-        s1 = ft_calloc(sizeof(char), 1);
-    if (!s2)
-        return (NULL);
-    j = 0;
-    str = malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1);
-    if (!str)
-        return (NULL);
-    i = 0;
-    while (s1[i])
-    {
-        str[i] = s1[i];
-        i++;
-    }
-    while (s2[j])
-    {
-        str[i + j] = s2[j];
-        j++;
-    }
-    str[i + j] = '\0';
-    return (str);
-} 
 
 char	*nbcat(int av, char **ac)
 {
@@ -52,10 +23,9 @@ char	*nbcat(int av, char **ac)
 	str_2 = NULL;
 	while (i < av)
 	{
-		printf("testt\n");
-		str_2 = ft_strjoin2(str, " ");
+		str_2 = ft_strjoin(str, " ");
 		free(str);
-		str = ft_strjoin2(str_2, ac[i]);
+		str = ft_strjoin(str_2, ac[i]);
 		free(str_2);
 		i++;
 	}
